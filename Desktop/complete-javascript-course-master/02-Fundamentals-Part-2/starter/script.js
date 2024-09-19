a1d1c3a3639c377eb088c51e5e8f0c886cc45a13
raghav4444 <2023bcs065@axiscolleges.in>
2024-09-20 00:26:47 +0530

    Done

--- Desktop/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ Desktop/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -77,7 +77,7 @@ function fruitProcessor(apples, oranges) {
 console.log(fruitProcessor(2, 3));
 
 */
-
+/*
 ///////////////////////////////////////
 // Reviewing Functions
 const calcAge = function (birthYeah) {
@@ -99,3 +99,52 @@ const yearsUntilRetirement = function (birthYeah, firstName) {
 
 console.log(yearsUntilRetirement(1991, "Jonas"));
 console.log(yearsUntilRetirement(1950, "Mike"));
+
+/*
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+// Test 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+}
+checkWinner(scoreDolphins, scoreKoalas);
+
+checkWinner(576, 111);
+
+// Test 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
+
+*/
+function average(a, b, c) {
+  return (a + b + c) / 3;
+}
+// const calAverage = average();
+// console.log(average(10, 20, 33));
+let AvgDolphins = average(85, 54, 41);
+let Avgkoalas = average(23, 34, 27);
+
+function checkwinner(AvgDolphins, Avgkoalas) {
+  if (AvgDolphins >= 2 * Avgkoalas) {
+    return "Dolphins Wins...";
+  } else if (Avgkoalas >= 2 * AvgDolphins) {
+    return "Koalas Wins...";
+  } else {
+    return "Match Draw";
+  }
+}
+const Winner = checkwinner(AvgDolphins, Avgkoalas);
+console.log(Winner);
